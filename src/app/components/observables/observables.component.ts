e2842c19600c244acc94446358616bdc1ac17d12
studentsc07 <fguydiscart@hotmail.com>
2019-01-03 14:30:30 +0100

    trois test

--- src/app/components/observables/observables.component.ts
+++ src/app/components/observables/observables.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Question } from './models/question';
 import { Observable } from 'rxjs';
+import {UserApiService} from './services/user-api.service';
+import {User} from './models/user';
 
 @Component({
   selector: 'app-observables',
@@ -9,11 +11,54 @@ import { Observable } from 'rxjs';
 })
 export class ObservablesComponent implements OnInit {
 
-    private question: Observable<Question[]>;
+    private _question: Observable<Question[]>;
+    private _users$: Observable<User[]>;
+    private _users: User[];
+    private _userOne: User[];
 
-    constructor() { }
+    get question(): Observable<Question[]> {
+        return this._question;
+    }
+
+    set question(value: Observable<Question[]>) {
+        this._question = value;
+    }
+
+    get users$(): Observable<User[]> {
+        return this._users$;
+    }
+
+    set users$(value: Observable<User[]>) {
+        this._users$ = value;
+    }
+
+    get users(): User[] {
+        return this._users;
+    }
+
+    set users(value: User[]) {
+        this._users = value;
+    }
+
+    get userOne(): User[] {
+        return this._userOne;
+    }
+
+    set userOne(value: User[]) {
+        this._userOne = value;
+    }
+
+    constructor(private service: UserApiService) { }
 
     ngOnInit() {
+        this.users$ = this.service.getAll();
+        this.users$.subscribe (
+            u => {
+                console.log(u);
+                this.userOne = u;
+            },
+            'error'
+        )
     }
 
 }
--- src/app/components/observables/services/user-api.service.ts
+++ src/app/components/observables/services/user-api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from '../models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,9 @@ export class UserApiService {
         return this._url;
     }
 
-    constructor(private httpClient: HttpClient) {}
+    constructor(private _httpClient: HttpClient) {}
+
+    getAll(): Observable<User[]> {
+        return this._httpClient.get<User[]>(this.url);
+    }
 }
